ec47e044b7b42bcd3830ca4259371db9a6b2d28a
NAM JAEHO <njh215@snu.ac.kr>
2022-08-26 00:15:07 +0900

    Add url launcher for problem

--- app/lib/user_view.dart
+++ app/lib/user_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lmnop/user_view_model.dart';
 import 'package:lmnop/constants.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class UserView extends StatelessWidget {
   late UserViewModel viewModel;
@@ -17,44 +18,90 @@ class UserView extends StatelessWidget {
   Widget _buildUserViewBody(BuildContext context) {
     return Container(
       width: double.infinity,
-      padding: EdgeInsets.fromLTRB(40, 20 + MediaQuery.of(context).padding.top, 40, 20),
+      padding: EdgeInsets.fromLTRB(
+          40, 20 + MediaQuery.of(context).padding.top, 40, 20),
       child: Column(
         children: [
-          Divider(color: Theme.of(context).primaryColor,),
+          Divider(
+            color: Theme.of(context).primaryColor,
+          ),
           SizedBox(
             width: double.infinity,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 _buildUserID(context),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 _buildSolvedCount(),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 _buildMaxStreak(),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 _buildClass(),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 _buildTierAndRating(),
-                const SizedBox(height: 40,),
+                const SizedBox(
+                  height: 40,
+                ),
               ],
             ),
           ),
-          Divider(color: Theme.of(context).primaryColor,),
+          Divider(
+            color: Theme.of(context).primaryColor,
+          ),
           SizedBox(
             width: double.infinity,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 const Text("추천 문제"),
-                const SizedBox(height: 20,),
-                Text("문제 번호: ${viewModel.problem?.problemId ?? ""}"),
-                const SizedBox(height: 20,),
-                Text("문제 레벨: ${viewModel.problem?.level ?? ""}"),
-                const SizedBox(height: 40,),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text("문제 번호: ${viewModel.problem?.problemId ?? ""}",
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    )),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text("문제 레벨: ${viewModel.problem?.level ?? ""}",
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    )),
+                const SizedBox(
+                  height: 40,
+                ),
+                InkWell(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: viewModel.problem == null
+                        ? []
+                        : const [
+                            Text("문제 보러가기"),
+                            Icon(Icons.arrow_right),
+                          ],
+                  ),
+                  onTap: () => launchUrlString(
+                      "https://boj.kr/${viewModel.problem?.problemId ?? ""}"),
+                ),
               ],
             ),
           ),
@@ -69,8 +116,12 @@ class UserView extends StatelessWidget {
     return RichText(
       text: TextSpan(
         children: <TextSpan>[
-          TextSpan(text: "Hi, ", style: TextStyle(color: Theme.of(context).primaryColorDark)),
-          TextSpan(text: viewModel.user?.handle ?? "", style: TextStyle(color: Theme.of(context).primaryColor)),
+          TextSpan(
+              text: "Hi, ",
+              style: TextStyle(color: Theme.of(context).primaryColorDark)),
+          TextSpan(
+              text: viewModel.user?.handle ?? "",
+              style: TextStyle(color: Theme.of(context).primaryColor)),
         ],
         style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
       ),
@@ -81,9 +132,13 @@ class UserView extends StatelessWidget {
     return RichText(
       text: TextSpan(
         children: <TextSpan>[
-          const TextSpan(text: "총 문제 해결: ", style: TextStyle(fontWeight: FontWeight.w300)),
-          TextSpan(text: "${viewModel.user?.solvedCount ?? ""}", style: const TextStyle(fontWeight: FontWeight.w700)),
-          const TextSpan(text: "문제", style: TextStyle(fontWeight: FontWeight.w300)),
+          const TextSpan(
+              text: "총 문제 해결: ", style: TextStyle(fontWeight: FontWeight.w300)),
+          TextSpan(
+              text: "${viewModel.user?.solvedCount ?? ""}",
+              style: const TextStyle(fontWeight: FontWeight.w700)),
+          const TextSpan(
+              text: "문제", style: TextStyle(fontWeight: FontWeight.w300)),
         ],
         style: const TextStyle(fontSize: 20, color: Colors.black),
       ),
@@ -94,9 +149,14 @@ class UserView extends StatelessWidget {
     return RichText(
       text: TextSpan(
         children: <TextSpan>[
-          const TextSpan(text: "최대 연속 문제 해결: ", style: TextStyle(fontWeight: FontWeight.w300)),
-          TextSpan(text: "${viewModel.user?.maxStreak ?? ""}", style: const TextStyle(fontWeight: FontWeight.w700)),
-          const TextSpan(text: "일", style: TextStyle(fontWeight: FontWeight.w300)),
+          const TextSpan(
+              text: "최대 연속 문제 해결: ",
+              style: TextStyle(fontWeight: FontWeight.w300)),
+          TextSpan(
+              text: "${viewModel.user?.maxStreak ?? ""}",
+              style: const TextStyle(fontWeight: FontWeight.w700)),
+          const TextSpan(
+              text: "일", style: TextStyle(fontWeight: FontWeight.w300)),
         ],
         style: const TextStyle(fontSize: 20, color: Colors.black),
       ),
@@ -107,10 +167,15 @@ class UserView extends StatelessWidget {
     return RichText(
       text: TextSpan(
         children: <TextSpan>[
-          TextSpan(text: "${tierToTierName[viewModel.user?.tier] ?? ""} ${viewModel.user?.rating ?? ""}",
+          TextSpan(
+              text:
+                  "${tierToTierName[viewModel.user?.tier] ?? ""} ${viewModel.user?.rating ?? ""}",
               style: tierEnumToTextStyle[tierToTierEnum[viewModel.user?.tier]]),
         ],
-        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w700,),
+        style: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+        ),
       ),
     );
   }
@@ -119,10 +184,16 @@ class UserView extends StatelessWidget {
     return RichText(
       text: TextSpan(
         children: <TextSpan>[
-          TextSpan(text: "${classToClassName[viewModel.user?.classNumber] ?? ""} ${classDecorationToClassDecorationName[viewModel.user?.classDecoration] ?? ""}",
-            style: classEnumToTextStyle[classToClassEnum[viewModel.user?.classNumber]]),
+          TextSpan(
+              text:
+                  "${classToClassName[viewModel.user?.classNumber] ?? ""} ${classDecorationToClassDecorationName[viewModel.user?.classDecoration] ?? ""}",
+              style: classEnumToTextStyle[
+                  classToClassEnum[viewModel.user?.classNumber]]),
         ],
-        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w700,),
+        style: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+        ),
       ),
     );
   }
